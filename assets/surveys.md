1317068502ee6dcbe322b1547029da18a1b1d434
Renan Greca <renangreca@GeneralKenobi.lan>
2022-02-10 11:01:24 +0100

    Added secondary studies list; updated GitHub workflow

---
+++ assets/surveys.md
@@ -0,0 +1,21 @@
+---
+# Feel free to add content and custom Front Matter to this file.
+# To modify the layout, see https://jekyllrb.com/docs/themes/#overriding-theme-defaults
+
+layout: secondaries
+title: Secondaries
+---
+
+Below is a table listing papers on the topic of software regression testing that satisfy Industrial Relevance and Applicability criteria.
+The criteria and initial selection process is detailed in the paper "State of Practical Applicability of Regression Testing Research: A Live Systematic Literature Review".
+For further information, please read the [About](about) and [Contribute](contribute) pages.
+
+Legend:
+* Year: publication year of the paper, according to the publisher.
+* Authors: list of authors, following the format "LastName1, FirstName1; LastName2, FirstName2; ...".
+* Title: title of the paper linking to a page with further information.
+* TCP: Test Case Prioritization
+* TCS: Test Case Selection
+* TSR: Test Suite Reduction
+* TSA: Test Suite Augmentation
+
---
+++ support/process_csv.py
@@ -0,0 +1,94 @@
+# coding=utf8
+
+# Receives a CSV file from our Google Sheet and re-organizes the data that should be displayed on the website.
+
+import csv
+import json
+
+papers = []
+with open("Secondary Study - Final List.csv") as input_file:
+    reader = csv.reader(input_file) #, delimiter=',', quitechar='"')
+    next(reader)
+    next(reader)
+    for row in reader:
+        paper = {}
+
+        paper['year'] = row[0]
+        paper['authors'] = row[1]
+        paper['title'] = row[2]
+        paper['bibtex'] = row[3]
+        paper['abstract'] = row[4]
+        paper['published_in'] = row[5]
+        paper['publisher'] = row[6]
+        paper['doi'] = row[8]
+        paper['date'] = row[9]
+
+        paper['tcp'] = row[17]
+        paper['tcs'] = row[18]
+        paper['tsr'] = row[19]
+        paper['tsa'] = row[20]
+
+        paper['ind_motivation'] = row[23]
+        paper['ind_evaluation'] = row[24]
+        paper['exp_subjects'] = row[25]
+        paper['ind_partner'] = row[26]
+        paper['ind_author'] = row[27]
+        paper['prac_feedback'] = row[28]
+        paper['avai_tool'] = row[29]
+        paper['put_practice'] = row[30]
+
+        paper['suppl_url'] = row[31]
+
+        paper['approach'] = row[34]
+        paper['metrics'] = row[35]
+        paper['open_challenges'] = row[36]
+        
+        papers.append(paper)
+
+for paper in papers:
+    with open("../_data/jsons/"+paper['bibtex']+".json", "w") as output_file:
+        output_file.write(json.dumps(paper))
+
+with open("../_data/output.json", "w") as output_file:
+    output_file.write(json.dumps(papers))
+
+papers = []
+with open("Tertiary Study - Data Collection.csv") as input_file:
+    reader = csv.reader(input_file) #, delimiter=',', quitechar='"')
+    next(reader)
+    next(reader)
+    for row in reader:
+        paper = {}
+
+        paper['year'] = row[0]
+        paper['authors'] = row[1]
+        paper['title'] = row[2]
+        paper['bibtex'] = row[7]
+        paper['abstract'] = row[3]
+        paper['published_in'] = row[4]
+        paper['publisher'] = row[5]
+        paper['doi'] = row[8]
+        paper['date'] = row[9]
+
+        paper['tcp'] = row[15]
+        paper['tcs'] = row[16]
+        paper['tsr'] = row[17]
+        paper['tsa'] = row[18]
+
+        paper['paper_count'] = row[19]
+        paper['years_covered'] = row[21]
+        paper['systematic'] = row[22]
+
+        paper['approach'] = row[27]
+        paper['metrics'] = row[28]
+        paper['conclusions'] = row[29]
+        paper['open_challenges'] = row[30]
+        
+        papers.append(paper)
+
+for paper in papers:
+    with open("../_data/secondary_jsons/"+paper['bibtex']+".json", "w") as output_file:
+        output_file.write(json.dumps(paper))
+
+with open("../_data/secondaries.json", "w") as output_file:
+    output_file.write(json.dumps(papers))

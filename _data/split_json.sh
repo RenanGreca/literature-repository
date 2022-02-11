926d28f239015739b056de12a366b6c04e0fee92
Renan Greca <renangreca@gmail.com>
2022-02-11 14:45:03 +0100

    hopefully fixed CSS url issue

--- _data/split_json.sh
+++ _data/split_json.sh
@@ -1,5 +1,5 @@
-jq -cr 'keys[] as $k | "\($k)\n\(.[$k])"' secondary_data_json.json | while read -r key; do
-  fname=$(jq --raw-output ".[$key].bibtex" secondary_data_json.json)
+jq -cr 'keys[] as $k | "\($k)\n\(.[$k])"' output.json | while read -r key; do
+  fname=$(jq --raw-output ".[$key].bibtex" output.json)
   read -r item
-  printf '%s\n' "$item" > "./jsons/$fname.json"
+  printf '%s\n' "$item" > "./primary_jsons/$fname.json"
 done
